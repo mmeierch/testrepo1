9ebbef2822488dbec58c65ea826c3df026918ac7
Zisch <zisch@dals.ch>
2014-09-01 16:09:06 +0200

    playing with XML catalogs

--- src/main/java/zisch/Test.java
+++ src/main/java/zisch/Test.java
@@ -1,8 +1,31 @@
 package zisch;
 
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+
 public class Test {
-  public Test () {
-    super();
+
+  public static void main (final String[] args) throws Exception {
+    System.setProperty("java.net.useSystemProxies", "true");
+    final URL url = new URL("http://www.w3.org/2001/xml.xsd");
+    final File outfile = new File(
+            "C:\\UBS\\Dev\\t471091\\prj\\testrepo1\\src\\main\\resources\\zisch\\catalog\\xml\\xml.xsd");
+    final InputStream is = url.openStream();
+    final OutputStream os = new BufferedOutputStream(new FileOutputStream(outfile));
+    final byte[] buf = new byte[8 * 1024];
+    try {
+      for (int cnt = is.read(buf); cnt > 0; cnt = is.read(buf)) {
+        os.write(buf, 0, cnt);
+      }
+    } finally {
+      os.close();
+      is.close();
+    }
   }
 }
